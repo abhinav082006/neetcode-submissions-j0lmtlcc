0247177dadc7e8458573ce1e961afc03a5c7eccf
abhinav082006 <abhinavgadipelli0806@gmail.com>
2026-04-18 23:59:43 +0530

    Add: binary-tree-postorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def postorderTraversal(self, root):
+        result = []
+        
+        def dfs(node):
+            if not node:
+                return
+            dfs(node.left)     # Left
+            dfs(node.right)    # Right
+            result.append(node.val)  # Root
+        
+        dfs(root)
+        return result
